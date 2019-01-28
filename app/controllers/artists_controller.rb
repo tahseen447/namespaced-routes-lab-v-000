de11d763df1da4857703f5f9fb3c1ae9a2f26b12
tahseen447 Siddiqui <tahseenfatima1986@gmail.com>
2019-01-28 20:53:13 +0000

    Done.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,6 +1,13 @@
 class ArtistsController < ApplicationController
+  before_action :set_preferences, only: [:index, :new]
+
+
   def index
-    @artists = Artist.all
+    if @preferences && @preferences.artist_sort_order
+     @artists = Artist.order(name: @preferences.artist_sort_order)
+   else
+     @artists = Artist.all
+   end
   end
 
   def show
@@ -8,7 +15,11 @@ class ArtistsController < ApplicationController
   end
 
   def new
+    if @preferences && !@preferences.allow_create_artists
+    redirect_to artists_path
+  else
     @artist = Artist.new
+  end
   end
 
   def create
@@ -49,4 +60,8 @@ class ArtistsController < ApplicationController
   def artist_params
     params.require(:artist).permit(:name)
   end
+
+  def set_preferences
+  @preferences = Preference.first
+end
 end
